24984d61f990392b3d708d1789253e9af7386acd
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2021-10-18 13:42:00 +0000

    Project update

--- File_compression/main.c
+++ File_compression/main.c
@@ -1,20 +1,44 @@
 #include "main.h"
-#define BFRSIZE 100
-//test
-int main(void)
+
+
+#if 1
+int main(int argc, char **argv)
 {
-    FILE *fstream = NULL;
-    long flen = 0;
-    char buffer[BFRSIZE];
-    uint8_t *data = NULL;
-    read_str("Enter file name, please: ", buffer, BFRSIZE);
-    fstream = open_file(buffer, "rb");
-    flen = file_size(fstream);
-    data = (uint8_t*) malloc(flen);
-    printf("%u\n", crc16(data, flen));
-    free(data);
-    fclose(fstream);
+    FILE *fin = NULL, *fout = NULL;
+    *argv = basename(*argv);
+    if(argc != 3) {
+        fprintf(stderr, "%s: invalid number of arguments: ", argv[0]);
+	    fprintf(stderr, "%d detected. Should be 2.\n%s\n", argc-1, USE_MSG);
+        return (-1);
+    } else if(!strcmp(argv[1], "pack")) {
+
+        fin = open_file(argv[2], "rb");
+        char *fname = strtok(argv[2], ".");
+        fout = open_file(strcat(fname, ".rle"), "wb");
+        rle_pack(fin, fout);
+        printf("crc is %X\n", crc16(fin));
+        fclose(fin);
+        fclose(fout);
+
+    } else if(!strcmp(argv[1], "unpack")) {
+
+        fin = open_file(argv[2], "rb");
+        char *fname = strtok(argv[2], ".");
+        fout = open_file(strcat(fname, ".new"), "w");
+        rle_unpack(fin, fout);
+        fclose(fin);
+        fclose(fout);
+    }
+    else {
+        fprintf(stderr, "%s: invalid comand: %s ", argv[0], argv[1]);
+	    fprintf(stderr, "detected. Should be 2.\n%s\n", USE_MSG);
+        return (-1);
+    }
+
+
 }
+#endif
+
 long file_size(FILE *fp)
 {
     long flen = 0;
@@ -33,15 +57,13 @@ FILE * open_file(char *fname, char *mode)
     }
     return fp;
 }
-void read_str(const char *prompt, char *buffer, int size)
+
+uint16_t crc16(FILE *fp)
 {
-    printf("%s", prompt);
-    fgets(buffer, size, stdin);
-    if(buffer[strlen(buffer)-1] == '\n') {
-        buffer[strlen(buffer)-1] = '\0';
-    }
-}
-uint16_t crc16(const uint8_t* data_p, unsigned int length) {
+    long length = file_size(fp);
+    uint8_t *data_p = (uint8_t*) malloc(length+1);
+    read_file(fp, data_p, length);
+
     uint8_t x;
     uint16_t crc = 0xFFFF;
 
@@ -57,4 +79,65 @@ int read_file(FILE *fp, uint8_t *data, long flen)
     int count = 0;
     count = fread(data, sizeof(uint8_t), flen, fp);
     return count;
+}
+
+int rle_pack(FILE *fin, FILE *fout)
+{
+    int c, pc = ASCIIEND;
+    unsigned char n = 0;
+
+    while(1) {
+        c = getc(fin);
+        if (c == pc && n < ASCIIEND-1)
+            n++;
+        else {
+            if (n > 3) {
+                putc(DELIM, fout);
+                putc(pc, fout);
+                putc(n, fout);
+            } else {
+                if(n >0 && n <=3)
+                    putc(pc, fout);
+            }
+            if (c == DELIM) {
+                putc(c, fout);
+                putc(c, fout);
+                n = 0;
+                pc = ASCIIEND;
+                continue;
+            } else if (c == EOF)
+                break;
+            else {
+                n = 1;
+                pc = c;
+            }
+        }
+    }
+    return 0;
+}
+int rle_unpack(FILE *fin, FILE *fout)
+{
+    int c, rc = ASCIIEND;
+    bool repeat = false;
+
+    while ((c = getc(fin)) != EOF) {
+        if (repeat) {
+            if (rc == ASCIIEND) {
+                if (c  == DELIM) {
+                    putc(DELIM, fout);
+                    repeat = false;
+                } else
+                    rc = c;
+            } else {
+                for (int i = 0; i < c; i++)
+                    putc(rc, fout);
+                repeat = false;
+                rc = ASCIIEND;
+            }
+        } else if (c != DELIM)
+            putc(c, fout);
+        else
+            repeat = true;
+    }
+    return 0;
 }
--- File_compression/main.h
+++ File_compression/main.h
@@ -5,10 +5,21 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <libgen.h>
+
+#define BFRSIZE 500
+#define ASCIIEND 0x100
+#define DELIM 0x80
+#define USE_MSG "Usage of the program:\n" \
+        "rle pack filename (compresses the specified file)\n" \
+        "rle unpack filename (decompresses the specified file)"
+
 
 FILE * open_file(char *fname, char *mode);
 void read_str(const char *prompt, char *buffer, int size);
 
-uint16_t crc16(const uint8_t* data_p, unsigned int length);
+uint16_t crc16(FILE *fp);
 long file_size(FILE *fp);
 int read_file(FILE *fp, uint8_t *data, long flen);
+int rle_pack(FILE *fin, FILE *fout) ;
+int rle_unpack(FILE *fin, FILE *fout);
